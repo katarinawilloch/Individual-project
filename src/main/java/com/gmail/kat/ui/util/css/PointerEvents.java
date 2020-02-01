58a19cfac624b68d43bf92cc68d16349bb46bb3b
Katarina Willoch <katarina.willoch@kcl.ac.uk>
2020-02-01 18:07:14 +0000

    Initial code base

---
+++ src/main/java/com/gmail/kat/ui/util/css/PointerEvents.java
@@ -0,0 +1,16 @@
+package com.gmail.kat.ui.util.css;
+
+public enum PointerEvents {
+
+	AUTO("auto"), NONE("none");
+
+	private String value;
+
+	PointerEvents(String value) {
+		this.value = value;
+	}
+
+	public String getValue() {
+		return value;
+	}
+}
